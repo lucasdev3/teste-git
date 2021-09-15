3faf9f8d3fbe1646b210df4fc73bef272b2beeeb
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-09-15 12:13:48 -0300

    atualizacao
    incluindo condicionais

--- teste.py
+++ teste.py
@@ -1,4 +1,15 @@
 nome = input("Nome: ")
 idade = int(input("Idade: "))
 
-print(f"Ola {nome}, você possui {idade} anos")
+
+
+if idade <= 0:
+    print("Idade invalida...")
+elif idade < 18:
+    print(f"Ola {nome}, você possui {idade} anos")
+    print("Você é menor de idade")
+else:
+    print(f"Ola {nome}, você possui {idade} anos")
+    print("Voce é maior de idade")
+    
+    
